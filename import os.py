c20a507f5b82c7afebe6876f0b83341b61c13cc8
AirDataSolutions <91428527+AirDataSolutions@users.noreply.github.com>
2021-09-28 22:27:12 -0500

    Python Filename List
    Python Filename List

---
+++ import os.py
@@ -0,0 +1,13 @@
+import os
+
+path = 'c:\\temp\\elevation\\lidar_laz' #Change to Input Folder Destination for LiDAR Data. 
+										#Right click the foler copy adress as text and paste, MAKE SURE THERES TWO \\'s
+files = os.listdir(path)
+
+print("The length of the list is: ", len(files))
+for f in files:
+	print(f)
+
+	#For loop needed to compare the length of the list every few seconds until they equal eachother a required number of times
+	#to ensure all files have been placed and are ready to be processed together
+	#Alternative: have an empty .txt file called Go.txt and when that file is seen in the list the rest of the process will continue 
