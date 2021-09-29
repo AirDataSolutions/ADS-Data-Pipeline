05de8bbc2d8ad6c60c55f21178a17d2271c50458
AirDataSolutions <91428527+AirDataSolutions@users.noreply.github.com>
2021-09-28 22:40:37 -0500

    Update import os.py

--- import os.py
+++ import os.py
@@ -11,3 +11,4 @@ for f in files:
 	#For loop needed to compare the length of the list every few seconds until they equal eachother a required number of times
 	#to ensure all files have been placed and are ready to be processed together
 	#Alternative: have an empty .txt file called Go.txt and when that file is seen in the list the rest of the process will continue 
+	#This could be valuable in future development in case we need input data for project to project it could be placed there.
