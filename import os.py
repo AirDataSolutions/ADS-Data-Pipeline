3125cbf7393fd889eff10c87c4ab7e7c70b4eb7c
AirDataSolutions <91428527+AirDataSolutions@users.noreply.github.com>
2021-09-29 08:34:57 -0500

    Update import os.py

--- import os.py
+++ import os.py
@@ -1,14 +1,24 @@
-import os
+#The code so far imports a list of all the files in a certain diretory that the user inputs. It then searches for a file that will be put in manually as a part of the workflow.
+#This file will be used to store the user inputs for later use. 
 
-path = 'c:\\temp\\elevation\\lidar_laz' #Change to Input Folder Destination for LiDAR Data. 
-										#Right click the foler copy adress as text and paste, MAKE SURE THERES TWO \\'s
+import os
+path = 'c:\\temp\\elevation\\lidar_laz' #Change to Input Folder Destination for LiDAR Data. Right click the foler copy adress as text and paste, MAKE SURE THERES TWO \\'s
 files = os.listdir(path)
 
-print("The length of the list is: ", len(files))
+#print("The length of the list is: ", len(files)) this was just testing that the function worked. 
+#The code below is storing the length of the list as an int and using a temp variable to compare itself for verification that the system has loaded in all files. 
+int num_files
+int temp
+num_files = len(files)
+
+#Printing out the list of files
 for f in files:
 	print(f)
 
-	#For loop needed to compare the length of the list every few seconds until they equal eachother a required number of times
-	#to ensure all files have been placed and are ready to be processed together
-	#Alternative: have an empty .txt file called Go.txt and when that file is seen in the list the rest of the process will continue 
-	#This could be valuable in future development in case we need input data for project to project it could be placed there.
+#Seeing if the go.txt file is in the list or it will not continue. @Jack make sure every part of the code from now on is properly indented.
+if(files.count("go.txt")>=1)
+
+
+
+
+	
